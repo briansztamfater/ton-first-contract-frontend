3a5310dc8b5fab9f8cdb7f39d5e6d081deb8ea70
Brian Sztamfater <brian09@gmail.com>
2025-03-22 11:35:06 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/first_contract_front_end/",
+  base: "/ton-first-contract-frontend/",
 });
